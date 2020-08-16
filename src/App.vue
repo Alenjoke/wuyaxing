9e31a58471869ef84e6a017ed93bc8cc71230167
Will <1076158377@qq.com>
2020-08-16 22:41:44 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <el-row>
-      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-    </el-row>
-    <div class="block">
-    <span class="demonstration">默认</span>
-    <el-slider v-model="value1"></el-slider>
-  </div>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="top">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <div style='display: flex;justify-content: center;' >欢迎!</div>
+      </div>
+      <div>{{ content }}</div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            content: '这是内容'
+        }
+    }
+};
+</script>
+
+<style scoped>
+.top {
+display: flex;justify-content: center;
+animation: move 2s both;
+}
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 480px;
+}
+
+@keyframes move 
+{
+0%{margin-top: 0px;}
+100%{margin-top: 200px;}
+}
+</style>
