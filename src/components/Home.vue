a47c7726a44a4425d06d5a174dc6fa207e10c146
Will <1076158377@qq.com>
2020-08-17 15:27:23 +0800

    add

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,26 +4,43 @@
       <div slot="header" class="clearfix">
         <div style='display: flex;justify-content: center;' >欢迎!</div>
       </div>
-      <div>{{ content }}</div>
+      <div id="test"></div>
+      <div class="btn">
+        <el-button>Let's go!</el-button>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import Tiper from "tiper-js"
+import '../../node_modules/tiper-js/lib/tiper-js.css'
+
 export default {
     name: 'Home',
     data() {
         return {
             content: '这是内容'
         }
+    },
+    mounted: function() {
+      let tiper = new Tiper(document.getElementById('test'))
+      tiper.beginTyping(); //Begins typing with default text and config.
+      
+      tiper.options= {
+        pauseTimeout: 500,
+        hesitation: 0.5
+      }
+      tiper.beginTyping();
+      tiper.line("我于杀戮之中绽放，亦如黎明中的花朵");
     }
 };
 </script>
 
 <style scoped>
 .top {
-display: flex;justify-content: center;
-animation: move 2s both;
+  display: flex;justify-content: center;
+  animation: move 1s both;
 }
 .text {
   font-size: 14px;
@@ -32,7 +49,12 @@ animation: move 2s both;
 .item {
   margin-bottom: 18px;
 }
-
+.btn {
+  margin-top: 30px;
+  justify-content: center;
+  display: flex;
+  animation: showBtn 1s 2s both;
+}
 .clearfix:before,
 .clearfix:after {
   display: table;
@@ -49,6 +71,12 @@ animation: move 2s both;
 @keyframes move 
 {
 0%{margin-top: 0px;}
-100%{margin-top: 200px;}
+70%{margin-top: 200px;}
+100%{margin-top: 180px;}
+}
+
+@keyframes showBtn {
+  0%{opacity: 0;}
+  100%{opacity: 1;}
 }
 </style>
